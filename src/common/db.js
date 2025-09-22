50a225fd1fcce7da875cfc598a7d41421ced79c7
Niko <nicolas.m.aburto@gmail.com>
2025-09-21 23:18:44 -0300

    Update db.js

--- src/common/db.js
+++ src/common/db.js
@@ -1,14 +1,15 @@
 import { MongoClient } from 'mongodb';
 
-const uri = 'mongodb+srv://ev3_express:Nicolas1@cluster-express.vvntfdl.mongodb.net/cine-db?retryWrites=true&w=majority&appName=cluster-express';
+const uri = "mongodb+srv://ev3_express:Nicolas1@cluster-express.vvntfdl.mongodb.net/cine-db?retryWrites=true&w=majority&appName=cluster-express&tls=true";
+
 const client = new MongoClient(uri);
 
 export async function connectToDatabase() {
   try {
     await client.connect();
-    console.log('Conectado a MongoDB');
+    console.log("Conectado a MongoDB Atlas");
   } catch (err) {
-    console.error('Error al conectar a MongoDB:', err);
+    console.error("Error al conectar a MongoDB:", err);
     throw err;
   }
 }
